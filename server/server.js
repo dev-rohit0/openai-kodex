9a30cbf1dd1a8098f6ae110a04711addb9671022
dev-rohit0 <rohit.codein@gmail.com>
2023-03-24 01:34:21 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -39,7 +39,6 @@ app.post('/', async (req, res) => {
             bot: response.data.choices[0].text
         })
     } catch (error) {
-        alert(error);
         res.status(500).send({error})
     }
 })
